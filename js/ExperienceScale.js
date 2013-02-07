a588f79b46b830f63d169718c08694525c155c8a
aakintil <aakintil@andrew.cmu.edu>
2013-02-07 14:35:15 -0500

    adding scaling for the whole experience page...now styling

---
+++ js/ExperienceScale.js
@@ -0,0 +1,86 @@
+
+// top left div
+var div1 = $('#experience .eight.columns.first').mouseenter(function()
+{ 
+    // cool trick for getting integers even if the value is auto
+    var z = ~~$(this).css('zIndex')
+    console.log(z)
+
+    $(this).animate({
+        transform: 'translateX(200px) translateY(100px) scale(1.5)', 
+    }, function(){
+        $(this).css({'z-index' : '5'
+    });
+}); 
+}).mouseleave(function()
+{
+    $(this).animate({
+        z:0,
+        transform: 'translateX(0px) translateY(0px) scale(1)'
+    });
+});
+
+// top right div
+var div1 = $('#experience .eight.columns.second').mouseenter(function()
+{ 
+    // cool trick for getting integers even if the value is auto
+    var z = ~~$(this).css('zIndex')
+    console.log(z)
+
+    $(this).animate({
+        transform: 'translateX(-200px) translateY(100px) scale(1.5)', 
+    }, function(){
+        $(this).css({'z-index' : '5'
+    });
+}); 
+}).mouseleave(function()
+{
+    $(this).animate({
+        z:0,
+        transform: 'translateX(0px) translateY(0px) scale(1)'
+    });
+});
+
+
+// bottom left div
+var div1 = $('#experience .eight.columns.third').mouseenter(function()
+{ 
+    // cool trick for getting integers even if the value is auto
+    var z = ~~$(this).css('zIndex')
+    console.log(z)
+
+    $(this).animate({
+        transform: 'translateX(200px) translateY(-100px) scale(1.5)', 
+    }, function(){
+        $(this).css({'z-index' : '5'
+    });
+}); 
+}).mouseleave(function()
+{
+    $(this).animate({
+        z:0,
+        transform: 'translateX(0px) translateY(0px) scale(1)'
+    });
+});
+
+
+// bottom right div
+var div1 = $('#experience .eight.columns.fourth').mouseenter(function()
+{ 
+    // cool trick for getting integers even if the value is auto
+    var z = ~~$(this).css('zIndex')
+    console.log(z)
+
+    $(this).animate({
+        transform: 'translateX(-200px) translateY(-100px) scale(1.5)', 
+    }, function(){
+        $(this).css({'z-index' : '5'
+    });
+}); 
+}).mouseleave(function()
+{
+    $(this).animate({
+        z:0,
+        transform: 'translateX(0px) translateY(0px) scale(1)'
+    });
+});
